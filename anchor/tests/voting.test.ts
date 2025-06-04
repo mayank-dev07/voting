9e972fa6f867b8a64f5a9b85f68f6cebc4634f0e
mayank-dev07 <devmayank.hts@gmail.com>
2025-06-04 19:51:37 +0530

    feat: implement voting program and remove counter program

---
+++ anchor/tests/voting.test.ts
@@ -0,0 +1,6 @@
+/// <reference types="jest" />
+import * as anchor from '@coral-xyz/anchor'
+import { Program } from '@coral-xyz/anchor'
+import { Keypair } from '@solana/web3.js'
+
+describe('voting', () => {})
