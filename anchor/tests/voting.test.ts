a28b2e0860da8735b259e8907638854ffe8df0a7
mayank-dev07 <devmayank.hts@gmail.com>
2025-06-04 20:13:36 +0530

    chore: update dependencies and adjust voting program version

--- anchor/tests/voting.test.ts
+++ anchor/tests/voting.test.ts
@@ -1,6 +1,36 @@
-/// <reference types="jest" />
+import { BankrunProvider, startAnchor } from 'anchor-bankrun'
 import * as anchor from '@coral-xyz/anchor'
 import { Program } from '@coral-xyz/anchor'
-import { Keypair } from '@solana/web3.js'
+import { Keypair, PublicKey } from '@solana/web3.js'
+import { Voting } from '../target/types/voting'
 
-describe('voting', () => {})
+const IDL = require('../target/idl/voting.json')
+
+const votingAddress = new PublicKey(IDL.address)
+
+describe('voting', () => {
+  it('initialize poll', async () => {
+    const context = await startAnchor(
+      '',
+      [
+        {
+          name: 'voting',
+          programId: votingAddress,
+        },
+      ],
+      [],
+    )
+
+    const provider = new BankrunProvider(context)
+    const votingProgram = new Program<Voting>(IDL, provider)
+
+    await votingProgram.methods
+      .initializePoll(
+        new anchor.BN(1),
+        new anchor.BN(0),
+        new anchor.BN(1749047739),
+        'who is the best player in the world?',
+      )
+      .rpc()
+  })
+})
